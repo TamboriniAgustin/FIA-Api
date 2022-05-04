66507078c20cf670994b58f554b6d8b278bee6c4
Agustín Tamborini <atamborinicriscueli@frba.utn.edu.ar>
2022-05-04 13:12:57 -0300

    Seteo inicial del nuevo proyecto

---
+++ src/main/java/api/fia/FiaApi.java
@@ -0,0 +1,51 @@
+package api.fia;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import api.fia.configurations.LocalDateConverter;
+import api.fia.configurations.LocalDateTimeConverter;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@SuppressWarnings("deprecation")
+@SpringBootApplication
+@EnableOpenApi
+@ComponentScan(basePackages = { "api.fia" })
+public class FiaApi implements CommandLineRunner {
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+            throw new ExitException();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplication(FiaApi.class).run(args);
+    }
+
+    @Configuration
+    static class CustomDateConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addFormatters(FormatterRegistry registry) {
+            registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+            registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
+        }
+    }
+
+    class ExitException extends RuntimeException implements ExitCodeGenerator {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public int getExitCode() {
+            return 10;
+        }
+
+    }
+}
---
+++ src/main/java/api/fia/controllers/MainController.java
@@ -0,0 +1,12 @@
+package api.fia.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String loadIndex() {
+		return "index";
+	}
+}
